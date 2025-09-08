dae1ea7b50e40b03771a67e9a381c153b5e7536c
ROBINMATHEW-rm <robinmathew619619@gmail.com>
2025-09-08 21:33:43 +0530

    Files

--- src/main/java/com/productservice/productservice/repository/ProductRepo.java
+++ src/main/java/com/productservice/productservice/repository/ProductRepo.java
@@ -2,6 +2,8 @@ package com.productservice.productservice.repository;
 
 import com.productservice.productservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepo extends JpaRepository<Product,Long> {
 }
--- src/main/java/com/productservice/productservice/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/productservice/productservice/service/Impl/ProductServiceImpl.java
@@ -5,16 +5,17 @@ import com.productservice.productservice.dto.ProductData;
 import com.productservice.productservice.entity.Product;
 import com.productservice.productservice.repository.ProductRepo;
 import com.productservice.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
     private final ProductAssembler productAssembler;
-    private final ProductRepo productRepo;
+    @Autowired
+    private ProductRepo productRepo;
 
-    public ProductServiceImpl(ProductAssembler productAssembler, ProductRepo productRepo) {
+    public ProductServiceImpl(ProductAssembler productAssembler) {
         this.productAssembler = productAssembler;
-        this.productRepo = productRepo;
     }
 
     @Override
